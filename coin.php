070a5a2ae842a6b4492b28e58f45d4eaacc57737
anru <ctx2002@gmail.com>
2015-07-25 21:26:41 +1200

    adding more examples.

--- coin.php
+++ coin.php
@@ -22,9 +22,7 @@ function coin_t()
 function throw_coins($coin_f_collection, $number = 1, $pre = "")
 {
     $number -= 1;
-   
-    foreach ($coin_f_collection as $coin_function) {
-        
+    foreach ($coin_f_collection as $coin_function) {    
         if ($number > 0) {
             $pre .= $coin_function();
             throw_coins($coin_f_collection, $number, $pre);
@@ -33,6 +31,39 @@ function throw_coins($coin_f_collection, $number = 1, $pre = "")
             echo $pre . $coin_function() . "\n";    
         }    
     }
+}
+
+function throw_coins_no_recusive($coin_f_collection, $number = 1, $pre = "")
+{
+    $stack = array();
+    while(true) {
+        $number -= 1;
+        foreach ($coin_f_collection as $coin_function) {
+            if ($number > 0) {
+                $pre .= $coin_function();
+                array_push($stack, array('n' => $number, 'p' => $pre, 'r' => substr($pre, 0, -1)));
+                break;
+            } else {
+                echo $pre . $coin_function() . "\n";
+                array_pop();
+            }
+        }
+    }    
+}
+
+function coin_a()
+{
+    return "a";
+}
+
+function coin_b()
+{
+    return "b";
+}
+
+function coin_c()
+{
+    return "c";
 }
 
 //main 
@@ -46,8 +77,22 @@ echo "\n";
 throw_coins($coin_f_collection, 3);
 echo "\n";
 
+$coin_f_collection = array("coin_a", "coin_b", "coin_c");
+throw_coins($coin_f_collection, 1);
+echo "\n";
+
+$coin_f_collection = array("coin_a", "coin_b", "coin_c");
+throw_coins($coin_f_collection,2);
+echo "\n";
+
+$coin_f_collection = array("coin_a", "coin_b", "coin_c");
+throw_coins($coin_f_collection,3);
+echo "\n";
+/*
 throw_coins($coin_f_collection, 4);
 echo "\n";
 
 throw_coins($coin_f_collection, 10);
 echo "\n";
+ * 
+ */
